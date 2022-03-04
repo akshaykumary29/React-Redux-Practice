158aaa78e3065a1fe70ca9eb8cfc97c850acf30a
akshay <akshay.yamgar29@gmail.com>
2022-03-04 22:56:13 +0530

    [akshay]: Added inc. and dec. counter using redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/index';
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <h1>Hello React-Redux</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+
+        <div>
+          <a title="Decrement" onClick={() => dispatch(decNumber())}><span> - </span></a>
+          <input name="quantity" type="text" value={myState} />
+          <a title='Increment' onClick={() => dispatch(incNumber())}><span> + </span></a>
+        </div>
+
+      </div>
+    </>
   );
 }
 
